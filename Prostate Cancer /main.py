6f6b8a7c4e4d3c9356510babeb03640251036a32
Mohamed-Hussain-Ahmed-Ibrahim <engineermohamedhussain@gmail.com>
2025-08-03 11:48:15 +0300

    Create main.py

---
+++ Prostate Cancer /main.py
@@ -0,0 +1,11 @@
+from prostate_modular.welecome_gui import Welecome
+from prostate_modular.main_window import MainWindow
+from prostate_modular.first_gui_data_entry import FirstDataEntryGUI
+from prostate_modular.second_gui_data_entry import SecondDataEntryGUI
+from prostate_modular.third_gui_data_entry import ThirdDataEntryGUI
+from prostate_modular.fourth_gui_data_entry import FourthDataEntryGUI
+
+
+if __name__ == "__main__":
+    app = Welecome()
+    app.mainloop()
